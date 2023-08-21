81f7824e4541c9cadd7602924ef976cd8fb34c33
raccon <minsungt@naver.com>
2023-08-21 14:12:59 +0900

    ignore csv

--- backend/src/api.py
+++ backend/src/api.py
@@ -85,4 +85,4 @@ api.add_resource(Inputs, '/simulator/inputs')
 api.add_resource(Outputs, '/simulator/outputs')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8010)
